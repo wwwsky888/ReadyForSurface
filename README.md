10e42bfe396c44d38e520578761e224095267f99
Albresky <78648318+Albresky@users.noreply.github.com>
2023-03-09 17:29:43 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
 # ReadyForSurface
-微软商城官翻版Surface Pro 8补货邮件通知
+微软商城·官翻版Surface Pro 8补货通知
+
+ - 本仓库用于候补**微软商城**·`官翻版Surface Pro 8`，当产品补货时，会通过邮件自动发送通知到指定邮箱。
+
+---
+
+## 使用方法
+
+- Fork本仓库
+- 设置Secrets
+   - 进入你Fork后仓库的Settings
+   - 添加Secrets
+     - `Name`：`conf`
+     - `Value`:
+  ```
+  '{"m_filter": [["5483", "5919"], ["5928"]], "senderMail": {"addr": "example@exapmle.com", "passwd": "ExamplePassWd", "host": "smtp.example.com", "port": "587"}, "rcvMails": ["example@qq.com"]}'
+  ```
+  - [说明]
+    - `m_filter`字段可选值（**非空**）：
+      - 第一个列表（颜色）
+        - `"5483"`：`亮铂金`
+        - `"5919"`：`石墨黑`
+      - 第二个列表（规格） 
+        - `"5926"`: `英特尔酷睿 i5/8GB/128GB`
+        - `"5927"`: `英特尔酷睿 i5/8GB/256GB`
+        - `"5928"`: `英特尔酷睿 i5/16GB/256GB`
+        - `"5929"`: `英特尔酷睿 i5/8GB/512GB`
+        - `"5930"`: `英特尔酷睿 i7/16GB/256GB`
+        - `"5931"`: `英特尔酷睿 i7/16GB/512GB`
+        - `"5932"`: `英特尔酷睿 i7/16GB/1TB`
+        - `"5933"`: `英特尔酷睿 i7/32GB/1TB`
+    - `senderMail`字段
+      - `"addr"`： 用于发送邮件通知的`邮箱地址`
+      - `"passwd"`：用于发送邮件通知的`邮箱密码`或`授权码`
+      - `"host"`：用于发送邮件通知的`邮箱SMTP服务器地址`
+      - `"port"`：用于发送邮件通知的`邮箱SMTP服务器端口`
+    - `rcvMails`字段（至少包含一个有效邮箱）
+      - `"example01@qq.com"`
+      - `"example02@outlook.com"`
+      - `···`
+
+
+ - 添加Actions
+    - 参考配置如下
+```
+name: GITHUB ACITON ReadyForSurface
+
+on:
+  push:
+    branches: [ "main" ]
+  pull_request:
+    branches: [ "main" ]
+  schedule:
+    - cron: 1 * * * *
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+      - name: 'Set up Python'
+        uses: actions/setup-python@v1
+        with:
+          python-version: 3.9
+      - name: 'Install requests'
+        run: pip install requests beautifulSoup4 yagmail
+      - name: 'Working'
+        env:
+          conf: ${{ secrets.CONF }}
+        run: python ./main.py
+```
+- 等待补货通知（邮件）
+
+
---
+++ main.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3.9
+# -*- coding: utf-8 -*-
+# @Author  : Loot at the stars
+# @Time    : 2023/3/9 17:05
+# @File    : main.py
+
+
+import json
+import os
+from datetime import datetime, timedelta
+
+import requests
+import yagmail
+from bs4 import BeautifulSoup
+
+
+def nowT():
+    return (datetime.utcnow() + timedelta(hours=8)).strftime('%Y/%m/%d %H:%M:%S')
+
+
+class SurfaceWatchDog:
+    def __init__(self):
+        self.stock_url = 'https://www.microsoftstore.com.cn/certified-refurbished-surface-pro-8-configurate'
+        self.attrDict = {'5483': '亮铂金',
+                         '5919': '石墨灰',
+                         '5926': '英特尔酷睿 i5/8GB/128GB',
+                         '5927': '英特尔酷睿 i5/8GB/256GB',
+                         '5928': '英特尔酷睿 i5/16GB/256GB',
+                         '5929': '英特尔酷睿 i5/8GB/512GB',
+                         '5930': '英特尔酷睿 i7/16GB/256GB',
+                         '5931': '英特尔酷睿 i7/16GB/512GB',
+                         '5932': '英特尔酷睿 i7/16GB/1TB',
+                         '5933': '英特尔酷睿 i7/32GB/1TB'}
+        self.session = requests.session()
+        self.m_filter = []
+        self.senderMail = {}
+        self.rcvMails = []
+        self.loadConf()
+        self.yag = yagmail.SMTP(user=self.senderMail['addr'], password=self.senderMail['passwd'],
+                                host=self.senderMail['host'],
+                                port=self.senderMail['port'], smtp_ssl=False)
+
+    def loadConf(self):
+        try:
+            conf = os.environ.get('conf')
+            print(conf)
+            if not conf:
+                print(nowT() + "|配置有误...exit()")
+                exit(-1)
+            jConf = json.loads(conf)
+            self.m_filter = jConf['m_filter']
+            self.senderMail = jConf['senderMail']
+            self.rcvMails = jConf['rcvMails']
+        except Exception as e:
+            print(nowT() + e)
+            exit(-1)
+
+    @staticmethod
+    def genItem(device, color, sku, price, onSale):
+        return {'device': device, 'color': color, 'sku': sku, 'price': price, 'onSale': onSale}
+
+    def query(self):
+        res = self.session.request(url=self.stock_url, method='get')
+        if res.status_code != 200:
+            print("Error to browser Microsoft Store.")
+        if res:
+            soup = BeautifulSoup(res.text, "html.parser")
+            script = soup.findAll('script', type='text/x-magento-init')[16]
+            tarScript = json.loads(script.text)
+            onSales = \
+                tarScript['[data-role=swatch-options]']['IsobarCommerce_BundleSwatches/js/swatch-renderer'][
+                    'jsonConfig'][
+                    'mainProducts']
+            return onSales
+
+    def getOnSale(self, devices: dict, isOnSale: bool, _filter: list = None):
+        onSales = []
+        types = ['30', '20', '15']
+        try:
+            for t in types:
+                for device in devices[t]:
+                    item = devices[t][device]
+                    if not isOnSale or item['is_saleable']:
+                        device = self.genItem(item['attributes']['specification'],
+                                              item['attributes']['color'],
+                                              item['sku'],
+                                              item['selection_price_value'],
+                                              item['is_saleable'])
+                        if not _filter or device['color'] in _filter[0] and device['device'] in _filter[1]:
+                            onSales.append(device)
+                return onSales
+
+        except Exception as e:
+            print(e)
+
+    def printDevice(self, devices: list):
+        print('%-22s%-6s\t%-10s\t%-20s\t%-12s' % ('查询时间', '有货', '颜色', '规格', '价格'))
+        for device in devices:
+            content = '%-22s\t%-6s\t\t%-10s%-20s\t%-12s' % (
+                nowT(), device['onSale'], self.attrDict[device['color']],
+                self.attrDict[device['device']], device['price'])
+            print(content)
+            self.notify(content)
+
+    def notify(self, txt):
+        self.yag.send(self.rcvMails, '微软Surface已补货', txt)
+
+
+if __name__ == '__main__':
+    dog = SurfaceWatchDog()
+    allDevices = dog.getOnSale(dog.query(), True, dog.m_filter)
+    dog.printDevice(allDevices)
